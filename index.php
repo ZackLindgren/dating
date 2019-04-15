83adfe46a0d567d77d0eee1019550e6fd9a868b0
Zack L <CSCzephyr@gmail.com>
2019-04-14 21:42:27 -0400

    Update controller to send correct view

--- index.php
+++ index.php
@@ -19,7 +19,8 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3 ->route('GET /', function() {
-    echo '<h1>Hello, world!</h1>';
+    $view = new Template();
+    echo $view ->render('views/home.html');
 });
 
 //Run fat free
