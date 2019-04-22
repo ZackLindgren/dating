002183fd0d474b5e3f5432218341166a4bc80ab3
Zack L <CSCzephyr@gmail.com>
2019-04-21 23:35:06 -0400

    Add first form file and a route

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
  * This page is the controller for the Dating project
  */
 
+// starting the session
+session_start();
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -17,10 +20,19 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+// Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
 //Define a default route
 $f3 ->route('GET /', function() {
     $view = new Template();
     echo $view ->render('views/home.html');
+});
+
+//Define the route to the start of the form
+$f3 ->route('GET /signup', function() {
+    $view = new Template();
+    echo $view ->render('views/form1.html');
 });
 
 //Run fat free
