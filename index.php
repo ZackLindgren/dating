980a818e99b09d2e14d2ebef225cfc82c197488c
Zack L <CSCzephyr@gmail.com>
2019-04-22 01:19:06 -0400

    Add new route to second part of the form

--- index.php
+++ index.php
@@ -30,9 +30,21 @@ $f3 ->route('GET /', function() {
 });
 
 //Define the route to the start of the form
-$f3 ->route('GET /signup', function() {
+$f3 ->route('POST /signup', function() {
     $view = new Template();
     echo $view ->render('views/form1.html');
+});
+
+//Define the route to the second part of the form
+$f3 ->route('POST /signup2', function() {
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['species'] = $_POST['species'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['number'] = $_POST['number'];
+
+    $view = new Template();
+    echo $view ->render('views/form2.html');
 });
 
 //Run fat free
